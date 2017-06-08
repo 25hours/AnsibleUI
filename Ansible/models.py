417c07d89aa716e93a5c38688dd06e2df6963281
25hours <shawnniu@yeah.net>
2017-06-08 23:37:39 +0800

    优化ansible部分的表结构

--- Ansible/models.py
+++ Ansible/models.py
@@ -5,11 +5,14 @@ from UserAuthentication.models import UserProfile
 class Task(models.Model):
     TaskId = models.AutoField(u'任务ID',primary_key=True,max_length=16)
     Name = models.CharField(u'任务名称',max_length=255)
-    Description = models.TextField(u'任务描述',blank=True,null=True,default='')
+    Description = models.TextField(u'任务描述',blank=True,null=True)
     Hosts = models.CharField(u'主机',max_length=1024)
     Command = models.CharField(u'执行命令',max_length=1024)
-    PlayBook = models.TextField(u'任务剧本')
-    Script = models.TextField(u'Shell脚本')
+    PlayBook = models.TextField(u'任务剧本',blank=True,null=True)
+    Script = models.TextField(u'Shell脚本',blank=True,null=True)
+
+    def __str__(self):
+        return u'%s,%s,%s,%s,%s,%s,%s' %(self.TaskId,self.Name,self.Description,self.Hosts,self.Command,self.PlayBook,self.Script)
 
     class Meta:
         verbose_name = ('任务')
@@ -18,8 +21,8 @@ class Task(models.Model):
 class TaskInfo(models.Model):
     class Meta:
         ordering = ['-TaskId']
-        verbose_name = ('任务信息')
-        verbose_name_plural = ('任务信息')
+        verbose_name = ('执行结果')
+        verbose_name_plural = ('执行结果')
     Status_Choices = (
         ('Success', u'成功'),
         ('Running', u'执行中'),
@@ -32,3 +35,6 @@ class TaskInfo(models.Model):
     Status = models.CharField(u'执行状态',choices=Status_Choices,max_length=32)
     Result_stdout = models.TextField(u'执行成功信息',blank=True,editable=False,default='',null=True)
     Result_stderr = models.TextField(u'执行失败信息',blank=True,editable=False,default='',null=True)
+
+    def __str__(self):
+        return u'%s,%s,%s,%s,%s,%s' %(self.TaskId,self.name,self.ExecuteTime,self.Status,self.Result_stdout,self.Result_stderr)
